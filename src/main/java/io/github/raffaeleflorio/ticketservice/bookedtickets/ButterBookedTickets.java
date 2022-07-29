1e0a449bcb023f09724bec8e2e1af72fee0e8e70
Raffaele Florio <raffaeleflorio@protonmail.com>
2022-07-29 11:03:04 +0200

    integrates Event.externalId in ButterBookedTickets

--- src/main/java/io/github/raffaeleflorio/ticketservice/bookedtickets/ButterBookedTickets.java
+++ src/main/java/io/github/raffaeleflorio/ticketservice/bookedtickets/ButterBookedTickets.java
@@ -2,14 +2,14 @@ package io.github.raffaeleflorio.ticketservice.bookedtickets;
 
 import io.github.raffaeleflorio.ticketservice.BookedTickets;
 import io.github.raffaeleflorio.ticketservice.Butter;
+import io.github.raffaeleflorio.ticketservice.Events;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
-import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.UUID;
 
 /**
@@ -21,40 +21,30 @@ import java.util.UUID;
 final class ButterBookedTickets implements BookedTickets {
 
   private final Butter butter;
-  private final DataSource dataSource;
+  private final Events events;
 
   /**
    * Builds sold tickets
    *
-   * @param butter     The Butter's API
-   * @param dataSource The datasource
+   * @param butter The Butter's API
+   * @param events The events
    */
-  ButterBookedTickets(@RestClient final Butter butter, final DataSource dataSource) {
+  ButterBookedTickets(@RestClient final Butter butter, final Events events) {
     this.butter = butter;
-    this.dataSource = dataSource;
+    this.events = events;
   }
 
   @Override
   public Uni<Void> add(final UUID ticket, final UUID participant, final UUID event) {
     return this.slug(event)
-      .onItem().transformToUni(slug -> this.createCollectionItem(ticket, participant, slug))
-      .onFailure().recoverWithNull();
+      .onItem().transformToUniAndMerge(slug -> this.createCollectionItem(ticket, participant, slug))
+      .onFailure().recoverWithCompletion()
+      .toUni();
   }
 
-  private Uni<String> slug(final UUID event) {
-    try (var preparedStatement = this.dataSource
-      .getConnection()
-      .prepareStatement("SELECT EXTERNAL_ID FROM EVENTS WHERE ORIGIN='BUTTER' AND ID=?")
-    ) {
-      preparedStatement.setObject(1, event);
-      var rs = preparedStatement.executeQuery();
-      if (rs.next()) {
-        return Uni.createFrom().item(rs.getString(1));
-      }
-      return Uni.createFrom().failure(new RuntimeException("Unable to get a event's slug"));
-    } catch (SQLException e) {
-      return Uni.createFrom().failure(new RuntimeException(e));
-    }
+  private Multi<String> slug(final UUID eventId) {
+    return this.events.event(eventId)
+      .onItem().transformToMultiAndMerge(event -> event.externalId("BUTTER"));
   }
 
   private Uni<Void> createCollectionItem(final UUID ticket, final UUID participant, final String slug) {
