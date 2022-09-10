1f77cb643e8fc4c20b43cb0e7151f68fe663ba17
Raffaele Florio <raffaeleflorio@protonmail.com>
2022-09-10 16:12:03 +0200

    integrates events template

--- src/main/java/io/github/raffaeleflorio/ticketservice/client/events/EventsResource.java
+++ src/main/java/io/github/raffaeleflorio/ticketservice/client/events/EventsResource.java
@@ -1,25 +1,39 @@
 package io.github.raffaeleflorio.ticketservice.client.events;
 
 import io.github.raffaeleflorio.ticketservice.Events;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 import io.smallrye.mutiny.Uni;
 import org.jboss.resteasy.reactive.RestResponse;
 
 import javax.json.JsonObject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.UUID;
 
 @Path("/events")
 public final class EventsResource {
 
   private final Events events;
+  private final Template eventsHTMLTemplate;
 
-  public EventsResource(final Events events) {
+  public EventsResource(final Events events, @Location("events.html") final Template eventsHTMLTemplate) {
     this.events = events;
+    this.eventsHTMLTemplate = eventsHTMLTemplate;
   }
 
   @GET
-  public Uni<JsonObject> upcomingEvents() {
+  @Produces(MediaType.APPLICATION_JSON)
+  public Uni<JsonObject> upcomingEventsAsJson() {
     return this.events.upcoming().asJsonObject();
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public Uni<TemplateInstance> upcomingEventsAsHtml() {
+    return this.events.upcoming().asJsonObject()
+      .onItem().transform(this.eventsHTMLTemplate::data);
   }
 
   @POST
